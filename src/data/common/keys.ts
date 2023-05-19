a47015adb43fe50f2ccd0f432b924911ae9a24e2
Finale <yeket@buaa.edu.cn>
2023-05-19 09:13:05 +0800

    Update keys.ts

--- src/data/common/keys.ts
+++ src/data/common/keys.ts
@@ -2,10 +2,10 @@
  * 腾讯地图API KEY，**限定使用域名**使用时需要修改
  * 申请地址：https://lbs.qq.com/dev/console/application/mine
  */
-export const TMAP_KEY = 'T2DBZ-JTO3F-2G7JB-NXUR7-SNY3O-7TBAM';
+export const TMAP_KEY = 'XXJBZ-FNQWD-TRU46-H6LCX-FTJNK-G3F45';
 
 /**
  * 和风天气API，需要自行申请
  * 申请地址：https://id.qweather.com/#/login
  */
-export const QWEATHER_KEY = 'ed95368f5ed4427f9ad8594d5b1a6850';
+export const QWEATHER_KEY = '12ac7975698444dba54e1524246bb504';
